b9f5a6406ca8e9ece20cfe3fff9f590e47bb4c52
Louis Morgan <louismorgan.dev@gmail.com>
2023-12-31 17:36:47 +0000

    fix mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://louisjmorgan.github.io",
+  base: "/supermassiveuk",
   integrations: [tailwind({ applyBaseStyles: false })],
 });
