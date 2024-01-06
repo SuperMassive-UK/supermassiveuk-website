914e9420af63de085f7613fdb449a108384e774d
Louis Morgan <louismorgan.dev@gmail.com>
2024-01-06 16:12:37 +0000

    homepage fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-
+const isProd = import.meta.env.PROD;
+const isDev = import.meta.env.DEV;
 // https://astro.build/config
 export default defineConfig({
   site: "https://louisjmorgan.github.io",
-  base: "/supermassiveuk",
+  base: isProd ? "/supermassiveuk" : "/",
   integrations: [tailwind({ applyBaseStyles: false })],
 });
