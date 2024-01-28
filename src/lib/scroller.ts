3f53ed8d47782930c18e2c9af668be558d1726fe
Louis Morgan <louismorgan.dev@gmail.com>
2024-01-28 19:00:39 +0000

    loconative scroll

--- src/lib/scroller.ts
+++ src/lib/scroller.ts
@@ -1,11 +1,11 @@
-import LocomotiveScroll from "locomotive-scroll";
+import LoconativeScroll from "loconative-scroll";
 console.log("hello");
-export default new LocomotiveScroll({
+export default new LoconativeScroll({
   el: document.querySelector("[data-scroll-container]"),
   smooth: true,
   touchMultiplier: 2,
   tablet: {
-    breakpoint: 0,
-    smooth: false,
+    breakpoint: 768,
+    smooth: true,
   },
 });
