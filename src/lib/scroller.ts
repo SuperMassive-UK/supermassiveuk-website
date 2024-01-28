7cf7978ce6596581aa6e2c780937d6d0ee4aee56
Louis Morgan <louismorgan.dev@gmail.com>
2024-01-28 21:41:26 +0000

    implement loconative scroll

--- src/lib/scroller.ts
+++ src/lib/scroller.ts
@@ -1,11 +1,9 @@
 import LoconativeScroll from "loconative-scroll";
-console.log("hello");
 export default new LoconativeScroll({
   el: document.querySelector("[data-scroll-container]"),
   smooth: true,
-  touchMultiplier: 2,
   tablet: {
     breakpoint: 768,
-    smooth: true,
+    smooth: false,
   },
 });
